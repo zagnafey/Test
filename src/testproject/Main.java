2b1eba4cb6c914b2bf87d82281a5d5c5a575824d
zagna <y.baranchuk@jelastic.com>
2018-05-10 22:12:26 +0300

    dev commit

--- src/testproject/Main.java
+++ src/testproject/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Computer comp = new Computer("Lenovo", 8000, 120, 2.0);
 
-       /* comp.setRam(4000);
-       comp.setName("ACER");
+        comp.setRam(4000);
+        comp.setName("ACER");
         comp.setHdd(500);
-        comp.setRam(100);*/
+        comp.setRam(100);
 
         comp.on();
         comp.load();
